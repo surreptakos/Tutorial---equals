3d5f2d46a74a5c2e1d92a25786bea4b9d2d1d88a
surreptakos <djgatsakos@gmail.com>
2015-06-11 21:14:55 -0500

    Creation

---
+++ App.java
@@ -0,0 +1,79 @@
+class Person {
+	private int id;
+	private String name;
+
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		// will output the package and class followed by
+		// the unique hashCode (identifier)
+		System.out.println(new Object());
+		
+		Person person1 = new Person(5, "Bob");
+		Person person2 = new Person(5, "Bob");
+		
+		
+		// == tells you if 2 references are pointing to the same object or not
+		System.out.println(person1.equals(person2));
+		
+		Double value1 = 7.2;
+		Double value2 = 7.2;
+		
+		System.out.println(value1.equals(value2));
+		
+		Integer num1 = 6;
+		Integer num2 = 6;
+		
+		System.out.println(num1 == num2);
+		
+		String text1 = "Hello";
+		String text2 = "Helloasdsd".substring(0,5);
+		
+		System.out.println(text2);
+		
+		System.out.println(text1 == text2);
+		
+		// Always use equals() on non-primitive types
+		
+	}
+
+}
